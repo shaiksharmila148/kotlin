9f69309f59ced400c326633bb42bd1d0e2e86f8c
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-16 17:20:57 +0530

    Initial commit

---
+++ src/main/kotlin/basics/FromJava.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class FromJava {
+    public static void main(String[] args) {
+        Student abdul = new Student("sharmila",123,876);
+        System.out.println(sharmi.getName());
+    }
+
+}
