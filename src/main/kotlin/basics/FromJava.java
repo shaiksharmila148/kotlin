59551a11905f2b5f5340c30cd3e2403add578f78
shaiksharmila148 <shaiksharmila148@gmail.com>
2021-11-17 10:07:56 +0530

    firstspring

--- src/main/kotlin/basics/FromJava.java
+++
@@ -1,9 +0,0 @@
-package basics;
-
-public class FromJava {
-    public static void main(String[] args) {
-        Student abdul = new Student("sharmila",123,876);
-        System.out.println(sharmi.getName());
-    }
-
-}
